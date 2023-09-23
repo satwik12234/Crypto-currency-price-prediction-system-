0669690d00fbe2aa197ffe5b1d98141c4f92a13d
Satwik Sangannagari <108742039+satwik12234@users.noreply.github.com>
2023-09-23 12:15:10 +0530

    Create Training&Testing.py

---
+++ loadingdataset.py/Training&Testing.py
@@ -0,0 +1,10 @@
+training_size=int(len(closedf)*0.60)
+test_size=len(closedf)-training_size
+train_data,test_data=closedf[0:training_size,:],closedf[training_size:len(closedf),:1]
+print("train_data: ", train_data.shape)
+print("test_data: ", test_data.shape)
+X_train =X_train.reshape(X_train.shape[0],X_train.shape[1] , 1)
+X_test = X_test.reshape(X_test.shape[0],X_test.shape[1] , 1)
+
+print("X_train: ", X_train.shape)
+print("X_test: ", X_test.shape)
