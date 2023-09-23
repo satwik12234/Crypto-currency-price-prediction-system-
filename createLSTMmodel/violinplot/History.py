08aeb951cb0bc706da4d56575cbe8c51e1bb7e8e
Satwik Sangannagari <108742039+satwik12234@users.noreply.github.com>
2023-09-23 12:19:22 +0530

    Create History.py

---
+++ createLSTMmodel/violinplot/History.py
@@ -0,0 +1,13 @@
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(loss))
+
+plt.plot(epochs, loss, 'r', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend(loc=0)
+plt.figure()
+
+
+plt.show()
