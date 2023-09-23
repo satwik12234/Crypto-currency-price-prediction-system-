4bfd9714db4a54ab9cdfaf50339c1d351e297354
Satwik Sangannagari <108742039+satwik12234@users.noreply.github.com>
2023-09-23 12:43:48 +0530

    Create Results.py

---
+++ loadingdataset.py/Results.py
@@ -0,0 +1,12 @@
+### Lets Do the prediction and check performance metrics
+train_predict=model.predict(X_train)
+test_predict=model.predict(X_test)
+train_predict.shape, test_predict.shape
+train_predict = scaler.inverse_transform(train_predict)
+test_predict = scaler.inverse_transform(test_predict)
+original_ytrain = scaler.inverse_transform(y_train.reshape(-1,1)) 
+original_ytest = scaler.inverse_transform(y_test.reshape(-1,1))
+print("Train data MSE: ", mean_squared_error(original_ytrain,train_predict))
+print("Test data MSE: ", mean_squared_error(original_ytest,test_predict))
+print("Train data R2 score:", r2_score(original_ytrain, train_predict))
+print("Test data R2 score:", r2_score(original_ytest, test_predict))
