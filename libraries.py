38fe8f67900a9eaf535ffb8d22fb728a5d22a5bc
Satwik Sangannagari <108742039+satwik12234@users.noreply.github.com>
2023-09-23 11:28:53 +0530

    Create libraries.py

---
+++ libraries.py
@@ -0,0 +1,12 @@
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import pandas as pd
+import seaborn as sns
+from pylab import rcParams
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from sklearn.preprocessing import MinMaxScaler
+import math
+from sklearn.metrics import mean_squared_error,r2_score
